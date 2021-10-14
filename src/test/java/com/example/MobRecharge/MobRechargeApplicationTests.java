63899ebcb04e623e6892728907cc4cfd9bc6ede0
Ishika Singla <ishika.singla@ad.fc.net>
2021-10-14 17:49:00 +0530

    initial commit

---
+++ src/test/java/com/example/MobRecharge/MobRechargeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MobRecharge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobRechargeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
