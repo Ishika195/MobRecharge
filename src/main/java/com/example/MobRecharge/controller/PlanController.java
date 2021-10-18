9970de32977093b3caa230c7368444729716efe1
Abhishek Pandey <43136439+abhishek776655@users.noreply.github.com>
2021-10-18 15:01:01 +0530

    CREATE: Plan Service and Plan Controller

--- src/main/java/com/example/MobRecharge/controller/PlanController.java
+++ src/main/java/com/example/MobRecharge/controller/PlanController.java
@@ -1,5 +1,78 @@
 package com.example.MobRecharge.controller;
 
-public class PlanController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.example.MobRecharge.entity.Plan;
+import com.example.MobRecharge.service.PlanService;
+
+@RestController
+@RequestMapping("/mobRecharge/v1/")
+public class PlanController {
+	@Autowired
+	PlanService planService;
+
+	@GetMapping("plan")
+	ResponseEntity<List<Plan>> getPlans() {
+		try {
+			List<Plan> plans = planService.getAllPlans();
+			return ResponseEntity.status(HttpStatus.OK).body(plans);
+		} catch (RuntimeException exc) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Plans Not Found", exc);
+		}
+	}
+
+	@GetMapping("plan/{id}")
+	ResponseEntity<Plan> getPlan(@PathVariable int id) {
+		try {
+			return ResponseEntity.status(HttpStatus.OK).body(planService.getPlan(id));
+
+		} catch (RuntimeException exc) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Plans Not Found", exc);
+		}
+	}
+	
+	@PostMapping("plan")
+	ResponseEntity<String>createPlan(@RequestBody Plan plan){
+		try {
+			planService.createPlan(plan);
+			return ResponseEntity.status(HttpStatus.CREATED).body("Plan Saved");
+		}  catch (RuntimeException exc) {
+			throw new ResponseStatusException(HttpStatus.CONFLICT, "Plans Not Saved", exc);
+		}
+	}
+	
+	@DeleteMapping("plan/{id}")
+	ResponseEntity<String>createPlan(@PathVariable int id){
+		try {
+			planService.deletePlan(id);
+			return ResponseEntity.status(HttpStatus.OK).body("Plan Deleted");
+		}  catch (RuntimeException exc) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Plan Not found", exc);
+		}
+	}
+	
+	@PutMapping("plan/{id}")
+	ResponseEntity<Plan> updatePlan(@PathVariable int id, @RequestBody Plan plan) {
+		System.out.println(plan);
+		try {
+			return ResponseEntity.status(HttpStatus.OK).body(planService.updatePlan(id,plan));
+
+		} catch (RuntimeException exc) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Plans Not Found", exc);
+		}
+	}
+	
 }
--- src/main/java/com/example/MobRecharge/service/PlanService.java
+++ src/main/java/com/example/MobRecharge/service/PlanService.java
@@ -1,5 +1,72 @@
 package com.example.MobRecharge.service;
 
-public class PlanService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.MobRecharge.entity.Plan;
+import com.example.MobRecharge.repository.PlanRepository;
+
+@Service
+public class PlanService {
+	@Autowired
+	PlanRepository planRepository;
+	
+	public List<Plan> getAllPlans(){
+		return planRepository.findAll();
+	}
+	
+	public void deletePlan(int id) {
+		if(id == 0) {
+			throw new RuntimeException();
+		}
+		planRepository.deleteById(id);
+	}
+	
+	public Plan createPlan(Plan plan) {
+		if(plan==null) {
+			throw new RuntimeException("Empty object");
+		}
+		return planRepository.save(plan);
+	}
+	
+	public Plan getPlan(int id) {
+		if(id == 0) {
+			throw new RuntimeException();
+		}
+		Plan plan  = planRepository.getById(id);
+		System.out.println(plan);
+		if(plan==null) {
+			throw new RuntimeException("plan not found");
+		}
+		return plan;
+	}
+	
+	public Plan updatePlan(int id,Plan plan) {
+		if(id==0 || plan == null) {
+			throw new RuntimeException();
+		}
+		Plan exsistingPlan  = planRepository.getById(id);
+		if(exsistingPlan==null) {
+			throw new RuntimeException("plan not found");
+		}
+		if(plan.getData()!=0.0f) {
+			exsistingPlan.setData(plan.getData());
+		}
+		if(plan.getValidity()!=0) {
+			exsistingPlan.setValidity(plan.getValidity());
+		}
+		if(plan.getCallMinutes()!=0) {
+			exsistingPlan.setCallMinutes(plan.getCallMinutes());
+		}
+		
+		if(plan.getSms()!=0) {
+			exsistingPlan.setSms(plan.getSms());
+		}
+		planRepository.save(exsistingPlan);
+		return exsistingPlan;
+		
+	}
+	
 }
