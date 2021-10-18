56d398e14657f3c38ebc4fa860d59f069806c397
Ishika195 <91590352+Ishika195@users.noreply.github.com>
2021-10-18 16:00:06 +0530

    Update: exceptions

--- src/main/java/com/example/MobRecharge/controller/OfferController.java
+++ src/main/java/com/example/MobRecharge/controller/OfferController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.example.MobRecharge.entity.Offer;
 import com.example.MobRecharge.service.OfferService;
@@ -24,31 +26,51 @@ public class OfferController {
 	OfferService offerService;
 
 	@GetMapping("/offers")
-	List<Offer> getAllOffers() {
-		return offerService.getOffers();
+	ResponseEntity<List<Offer>> getAllOffers() {
+		try {
+			return ResponseEntity.status(HttpStatus.OK).body(offerService.getOffers());
+		} catch (RuntimeException exception) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Offers Not Found", exception);
+		}
 	}
 
 	@ResponseStatus(code = HttpStatus.CREATED)
 	@PostMapping("/offer")
-	String addNewOffer(@RequestBody Offer offer) {
-		Integer id = offerService.saveOffer(offer);
-		return "Successfully added offer with id : " + id;
+	ResponseEntity<String> addNewOffer(@RequestBody Offer offer) {
+		try {
+			Integer id = offerService.saveOffer(offer);
+			return ResponseEntity.status(HttpStatus.CREATED).body("Successfully added offer with id : " + id);
+		} catch (RuntimeException exception) {
+			throw new ResponseStatusException(HttpStatus.CONFLICT, "Offer Not Saved", exception);
+		}
 	}
 
 	@GetMapping("/offers/{id}")
-	Offer getOffer(@PathVariable Integer id) {
-		return offerService.getOffer(id);
+	ResponseEntity<Offer> getOffer(@PathVariable Integer id) {
+		try {
+			return ResponseEntity.status(HttpStatus.OK).body(offerService.getOffer(id));
+		} catch (RuntimeException exception) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Offers Not Found", exception);
+		}
 	}
-	
+
 	@ResponseStatus(code = HttpStatus.NO_CONTENT)
 	@DeleteMapping("/offer/{id}")
-	String deleteOffer(@PathVariable Integer id) {
-		offerService.deleteOffer(id);
-		return "Successfully added offer with id : " + id;
+	ResponseEntity<String> deleteOffer(@PathVariable Integer id) {
+		try {
+			offerService.deleteOffer(id);
+			return ResponseEntity.status(HttpStatus.OK).body("Successfully added offer with id : " + id);
+		}catch(RuntimeException exception) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Offer Not found", exception);
+		}
 	}
 
 	@PutMapping("/offer/{id}")
-	Offer updateOffer(@PathVariable Integer id, @RequestBody Offer offer) {
-		return offerService.updateOffer(id, offer);
+	ResponseEntity<Offer> updateOffer(@PathVariable Integer id, @RequestBody Offer offer) {
+		try {
+			return ResponseEntity.status(HttpStatus.OK).body(offerService.updateOffer(id, offer));
+		}catch(RuntimeException exception) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Offers Not Found", exception);
+		}
 	}
 }
