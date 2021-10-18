0e6eb7ae34b1736cbb6f8566a232f76275710b90
Ishika195 <91590352+Ishika195@users.noreply.github.com>
2021-10-18 13:33:13 +0530

    update: status codes

--- src/main/java/com/example/MobRecharge/controller/OfferController.java
+++ src/main/java/com/example/MobRecharge/controller/OfferController.java
@@ -3,6 +3,7 @@ package com.example.MobRecharge.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.MobRecharge.entity.Offer;
@@ -26,6 +28,7 @@ public class OfferController {
 		return offerService.getOffers();
 	}
 
+	@ResponseStatus(code = HttpStatus.CREATED)
 	@PostMapping("/offer")
 	String addNewOffer(@RequestBody Offer offer) {
 		Integer id = offerService.saveOffer(offer);
@@ -36,15 +39,17 @@ public class OfferController {
 	Offer getOffer(@PathVariable Integer id) {
 		return offerService.getOffer(id);
 	}
-
+	
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
 	@DeleteMapping("/offer/{id}")
 	String deleteOffer(@PathVariable Integer id) {
 		offerService.deleteOffer(id);
 		return "Successfully added offer with id : " + id;
 	}
+
 	@PutMapping("/offer/{id}")
 	Offer updateOffer(@PathVariable Integer id, @RequestBody Offer offer) {
 		offer.setOfferId(id);
-		return offerService.updateOffer(id,offer);
+		return offerService.updateOffer(id, offer);
 	}
 }
