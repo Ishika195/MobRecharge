7f584a6170d4474eecbc9040de244394e4cefa99
Ishika195 <91590352+Ishika195@users.noreply.github.com>
2021-10-18 13:15:50 +0530

    Created: Offer service with get,post and delete operations

---
+++ src/main/java/com/example/MobRecharge/controller/OfferController.java
@@ -0,0 +1,44 @@
+package com.example.MobRecharge.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.MobRecharge.entity.Offer;
+import com.example.MobRecharge.service.OfferService;
+
+@RestController
+@RequestMapping("/MobRechargeApp/v1")
+public class OfferController {
+	@Autowired
+	OfferService offerService;
+
+	@GetMapping("/offers")
+	List<Offer> getAllOffers() {
+		return offerService.getOffers();
+	}
+
+	@PostMapping("/offer")
+	String addNewOffer(@RequestBody Offer offer) {
+		Integer id = offerService.saveOffer(offer);
+		return "Successfully added offer with id : " + id;
+	}
+
+	@GetMapping("/offers/{id}")
+	Offer getOffer(@PathVariable Integer id) {
+		return offerService.getOffer(id);
+	}
+
+	@DeleteMapping("/offer/{id}")
+	String deleteOffer(@PathVariable Integer id) {
+		offerService.deleteOffer(id);
+		return "Successfully added offer with id : " + id;
+	}
+}
