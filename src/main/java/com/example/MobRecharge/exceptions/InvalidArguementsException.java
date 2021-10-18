8ab64c2423f74e6271e3ee1dbd9512fe7d30840c
Abhishek Pandey <43136439+abhishek776655@users.noreply.github.com>
2021-10-18 15:56:39 +0530

    CREATE:custom exceptions
    Added some custom exceptions for resource not found and bad arguements in exception package

---
+++ src/main/java/com/example/MobRecharge/exceptions/InvalidArguementsException.java
@@ -0,0 +1,12 @@
+package com.example.MobRecharge.exceptions;
+
+public class InvalidArguementsException extends RuntimeException{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public InvalidArguementsException(String message){
+		super(message);
+	}
+}
